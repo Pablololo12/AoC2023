29976367be81cd01125e3e2fce8ef9804ff93c51
pablololo12 <pabloheralm@gmail.com>
2023-12-04 10:52:53 +0000

    Day 4

---
+++ 4/main.hs
@@ -0,0 +1,64 @@
+import Data.Char
+import Data.Text.IO
+import Data.Text as T
+import GHC.List as L
+import GHC.Base as B
+
+parsn :: Text -> Int
+parsn x = read (T.unpack x)
+
+parseNums :: Text -> [Int]
+parseNums x = L.map (parsn) (L.filter (not . T.null) (T.split (==' ') x))
+
+reduce :: [Int] -> [Int] -> [Int]
+reduce [] _ = []
+reduce (x:xs) y
+      | x `L.elem` y = x : reduce xs y
+      | otherwise = reduce xs y
+
+calculateScore :: [Int] -> [Int] -> Int
+calculateScore x y = if z==0 then 0 else 2^(z-1)
+    where z = L.length (reduce x y)
+
+checkLine :: Text -> Int
+checkLine x = calculateScore (parseNums (T.strip (L.head y))) (parseNums (T.strip (L.last y)))
+    where y = T.split (=='|') x
+
+removeid :: Text -> Int
+removeid x = checkLine (L.last (T.split (==':') x))
+
+doAlgo :: [Text] -> [Int]
+doAlgo x = B.map (removeid) x
+
+calculateScorev2 :: [Int] -> [Int] -> Int
+calculateScorev2 x y = L.length (reduce x y)
+
+checkLinev2 :: Text -> Int
+checkLinev2 x = calculateScorev2 (parseNums (T.strip (L.head y))) (parseNums (T.strip (L.last y)))
+    where y = T.split (=='|') x
+
+removeidv2 :: Text -> Int
+removeidv2 x = checkLinev2 (L.last (T.split (==':') x))
+
+accpar :: [(Int, Int)] -> Int
+accpar [] = 1
+accpar (x:xs) = (snd x) + (accpar xs)
+
+calcchild :: [(Int, Int)] -> Int -> (Int, Int)
+calcchild x y = (y, accpar (L.take (y) x))
+
+tree :: [Int] -> [(Int, Int)]
+tree [] = []
+tree (x:xs) = (calcchild y x) : y
+    where y = tree xs
+
+doAlgo1 :: [Text] -> [Int]
+doAlgo1 x = B.map (removeidv2) x
+
+doAlgov2 :: [Text] -> [Int]
+doAlgov2 x = snd (L.unzip(tree (doAlgo1 x)))
+
+main :: IO()
+main = do
+     content <- Data.Text.IO.readFile "input.txt"
+     print (L.foldl (+) 0 (doAlgov2 (T.lines content)))
