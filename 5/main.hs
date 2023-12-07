45bbeb680d42b47a224a3497ed835607cffbb53d
pablololo12 <pabloheralm@gmail.com>
2023-12-07 21:51:12 +0000

    Improved Day 5 part 2 solution

--- 5/main.hs
+++ 5/main.hs
@@ -31,12 +31,37 @@ cases x (t:ts)
 doAlgo :: [Text] -> [Int]
 doAlgo (x:xs) = cases (B.map (parsn) (L.drop 1 (T.split (==' ') x))) xs
 
-generate :: [Int] -> [Int]
+filtv2 :: [(Int,Int)] -> [Int] -> [(Int,Int)]
+filtv2 [] x = []
+filtv2 ((f,s):xs) (a:b:c:_)
+    | f<0 = (f,s) : filtv2 xs [a,b,c]
+    | (f>=b) && ((b+c)>=(f+s)) = (-((f-b)+a),s) : filtv2 xs [a,b,c]
+    | (f>=b) && (f<(b+c)) = [(-((f-b)+a),((b+c)-f)),((b+c),((f+s)-(b+c)))] ++ filtv2 xs [a,b,c]
+    | ((f+s)>=b) && ((f+s)<(b+c)) = [(f,(b-f)),(-a,((f+s)-b))] ++ filtv2 xs [a,b,c]
+    | (f<b) && ((f+s)>(b+c)) = [(f,(b-f)),((b+c),((f+s)-(b+c))),(-a,c)] ++ filtv2 xs [a,b,c]
+    | otherwise = (f,s) : filtv2 xs [a,b,c]
+
+filterallv2 :: [(Int,Int)] -> [Text] -> [(Int,Int)]
+filterallv2 x [] = x
+filterallv2 x (t:ts) = filterallv2 (filtv2 x n) ts
+    where n = B.map (parsn) (T.split (==' ') t)
+
+abspar :: (Int,Int) -> (Int,Int)
+abspar (a,b) = (abs a, b)
+
+casesv2 :: [(Int,Int)] -> [Text] -> [(Int,Int)]
+casesv2 x [] = x
+casesv2 x (t:ts)
+    | isDigit (T.head t) = casesv2 x ts
+    | otherwise = casesv2 (L.map (abspar) (filterallv2 x (L.takeWhile (headisdigit) ts))) ts
+
+generate :: [Int] -> [(Int,Int)]
 generate [] = []
-generate (a:b:xs) = (L.take b [a..]) ++ (generate xs)
+generate (a:b:xs) = (a,b) : (generate xs)
 
 doAlgov2 :: [Text] -> [Int]
-doAlgov2 (x:xs) = cases (generate (B.map (parsn) (L.drop 1 (T.split (==' ') x)))) xs
+doAlgov2 (x:xs) = B.map (fst) (casesv2 j xs)
+  where j = generate (B.map (parsn) (L.drop 1 (T.split (==' ') x)))
 
 main :: IO()
 main = do
