2cfcc0e7a2573c97b03ca8d575bd843aeeb3e7eb
Pablo Hernandez Almudi <pablo.hernandezalmudi@arm.com>
2023-12-08 14:26:14 +0000

    Day 8 Part 2

--- 8/main.hs
+++ 8/main.hs
@@ -7,33 +7,55 @@ import GHC.Base as B
 import Data.Map as M
 import Debug.Trace
 
--- List of instructions, Rest of instructions, accumulator, next step, map of steps -> result
-iter :: [Int] -> [Int] -> Int -> Int -> [(Int,Int)] -> Int
+iter :: [Int] -> [Int] -> Int -> Text -> Map Text (Text,Text) -> Int
 iter f [] c w m = iter f f c w m
 iter f (x:xs) c w m
-    | w == -1 = c
-    | x == 0 = iter f xs (c+1) (fst (m !! w)) m
-    | x == 1 = iter f xs (c+1) (snd (m !! w)) m
+    | w == pack("ZZZ") = c
+    | x == 0 = iter f xs (c+1) (fst (m ! w)) m
+    | x == 1 = iter f xs (c+1) (snd (m ! w)) m
+
+allFinishZ :: [Text] -> Bool
+allFinishZ [] = True
+allFinishZ (x:xs)
+    | (T.last x)=='Z' = allFinishZ xs
+    | otherwise = False
+
+applyMoves :: Int -> [Text] -> Map Text (Text, Text) -> [Text] -> [Text]
+applyMoves _ [] _ x = x
+applyMoves 0 (x:xs) m y = applyMoves 0 xs m (y++[(fst (m ! x))])
+applyMoves 1 (x:xs) m y = applyMoves 1 xs m (y++[(snd (m ! x))])
+
+-- Instructions, rest of inst, count, curIndx, Map -> final value
+iterv2 :: [Int] -> [Int] -> Int -> [Text] -> Map Text (Text,Text) -> Int
+iterv2 f [] c w m = iterv2 f f c w m
+iterv2 f (x:xs) c w m
+    | allFinishZ w = c
+    | otherwise = iterv2 f xs (c+1) (applyMoves x w m []) m
 
 maptoint :: String -> [Int]
 maptoint [] = []
 maptoint (x:xs) = j : maptoint xs
     where j = if x=='R' then 1 else 0
 
-prepLines :: [Text] -> Map Text Int -> [(Int, Int)]
-prepLines [] _ = []
-prepLines (x:xs) m = (m ! (L.head b), m ! (L.last b)) : prepLines xs m
-    where b = T.split (==',') (T.filter (\w -> ((w/='(') && (w/=')'))) (L.last c))
+prepLines :: [Text] -> [(Text, (Text, Text))]
+prepLines [] = []
+prepLines (x:xs) = (a,(L.head b, L.last b)) : prepLines xs
+    where a = L.head c
+          b = T.split (==',') (T.filter (\w -> ((w/='(') && (w/=')'))) (L.last c))
           c = T.split (=='=') (T.filter (/=' ') x)
 
 doAlgo :: [Text] -> Int
-doAlgo (x:xs) = iter j j 0 (n ! (pack "AAA")) w
+doAlgo (x:xs) = iter j j 0 (pack "AAA") w
     where j = maptoint (unpack x)
-          w = prepLines xs n
-          n = fromList ((L.filter (\w -> (fst w)/=(pack "ZZZ")) (L.zip  m [0..])) ++ [((pack "ZZZ"), -1)])
-          m = B.map (T.filter (/=' ')) (B.map (L.head . (T.split (=='='))) xs)
+          w = fromList (prepLines xs)
+
+doAlgov2 :: [Text] -> Int
+doAlgov2 (x:xs) = iterv2 j j 0 m w
+    where j = maptoint (unpack x)
+          m = L.filter (\y -> (T.last y)=='A') (B.map (T.filter (/=' ')) (B.map (L.head . (T.split (=='='))) xs))
+          w = fromList (prepLines xs)
 
 main :: IO()
 main = do
      content <- Data.Text.IO.readFile "input.txt"
-     print (doAlgo (L.filter (not . T.null) (T.lines content)))
+     print (doAlgov2 (L.filter (not . T.null) (T.lines content)))
