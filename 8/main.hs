f692ccbe818a73c5b9525c31976f064a4f2c40f5
Pablo Hernandez Almudi <pablo.hernandezalmudi@arm.com>
2023-12-08 09:40:25 +0000

    Day 8 part 1

---
+++ 8/main.hs
@@ -0,0 +1,37 @@
+import Data.Char
+import Data.Text.IO
+import Data.Text as T
+import Data.List as D
+import GHC.List as L
+import GHC.Base as B
+import Data.Map as M
+import Debug.Trace
+
+iter :: [Int] -> [Int] -> Text -> Map Text (Text,Text) -> Int
+iter f [] w m = iter f f w m
+iter f (x:xs) w m
+    | w == pack("ZZZ") = 0
+    | x == 0 = 1 + (iter f xs (fst (m ! w)) m)
+    | x == 1 = 1 + (iter f xs (snd (m ! w)) m)
+
+maptoint :: String -> [Int]
+maptoint [] = []
+maptoint (x:xs) = j : maptoint xs
+    where j = if x=='R' then 1 else 0
+
+prepLines :: [Text] -> [(Text, (Text, Text))]
+prepLines [] = []
+prepLines (x:xs) = (a,(L.head b, L.last b)) : prepLines xs
+    where a = L.head c
+          b = T.split (==',') (T.filter (\w -> ((w/='(') && (w/=')'))) (L.last c))
+          c = T.split (=='=') (T.filter (/=' ') x)
+
+doAlgo :: [Text] -> Int
+doAlgo (x:xs) = iter j j (L.head (T.split (==' ') (L.head xs))) w
+    where j = maptoint (unpack x)
+          w = fromList (prepLines xs)
+
+main :: IO()
+main = do
+     content <- Data.Text.IO.readFile "input.txt"
+     print (doAlgo (L.filter (not . T.null) (T.lines content)))
