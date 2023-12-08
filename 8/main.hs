5339e3a13db30c5e42b9718cb4ac8dd71b4c2a9c
Pablo Hernandez Almudi <pablo.hernandezalmudi@arm.com>
2023-12-08 13:16:54 +0000

    Day 8 part 1

--- 8/main.hs
+++ 8/main.hs
@@ -7,29 +7,31 @@ import GHC.Base as B
 import Data.Map as M
 import Debug.Trace
 
-iter :: [Int] -> [Int] -> Int -> Text -> Map Text (Text,Text) -> Int
+-- List of instructions, Rest of instructions, accumulator, next step, map of steps -> result
+iter :: [Int] -> [Int] -> Int -> Int -> [(Int,Int)] -> Int
 iter f [] c w m = iter f f c w m
 iter f (x:xs) c w m
-    | w == pack("ZZZ") = c
-    | x == 0 = iter f xs (c+1) (fst (m ! w)) m
-    | x == 1 = iter f xs (c+1) (snd (m ! w)) m
+    | w == -1 = c
+    | x == 0 = iter f xs (c+1) (fst (m !! w)) m
+    | x == 1 = iter f xs (c+1) (snd (m !! w)) m
 
 maptoint :: String -> [Int]
 maptoint [] = []
 maptoint (x:xs) = j : maptoint xs
     where j = if x=='R' then 1 else 0
 
-prepLines :: [Text] -> [(Text, (Text, Text))]
-prepLines [] = []
-prepLines (x:xs) = (a,(L.head b, L.last b)) : prepLines xs
-    where a = L.head c
-          b = T.split (==',') (T.filter (\w -> ((w/='(') && (w/=')'))) (L.last c))
+prepLines :: [Text] -> Map Text Int -> [(Int, Int)]
+prepLines [] _ = []
+prepLines (x:xs) m = (m ! (L.head b), m ! (L.last b)) : prepLines xs m
+    where b = T.split (==',') (T.filter (\w -> ((w/='(') && (w/=')'))) (L.last c))
           c = T.split (=='=') (T.filter (/=' ') x)
 
 doAlgo :: [Text] -> Int
-doAlgo (x:xs) = iter j j 0 (L.head (T.split (==' ') (L.head xs))) w
+doAlgo (x:xs) = iter j j 0 (n ! (pack "AAA")) w
     where j = maptoint (unpack x)
-          w = fromList (prepLines xs)
+          w = prepLines xs n
+          n = fromList ((L.filter (\w -> (fst w)/=(pack "ZZZ")) (L.zip  m [0..])) ++ [((pack "ZZZ"), -1)])
+          m = B.map (T.filter (/=' ')) (B.map (L.head . (T.split (=='='))) xs)
 
 main :: IO()
 main = do
