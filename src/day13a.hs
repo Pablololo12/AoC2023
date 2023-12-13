7c81c9399de6cb18088b8f5dc1008c147e55152b
pablololo12 <pabloheralm@gmail.com>
2023-12-13 08:13:53 +0000

    Start Day13

--- src/day13a.hs
+++ src/day13a.hs
@@ -1,6 +1,31 @@
 module Day13a (solve) where
 
-import Data.Text as T
+import qualified Data.Text as T
+import Debug.Trace
 
-solve :: Text -> Int
-solve x = 0
+checkMirrorAux :: [[Char]] -> [[Char]] -> Int
+checkMirrorAux (a:as) (b:bs)
+    | a==b = checkMirrorAux as bs
+    | otherwise = -1
+
+checkMirror :: [[Char]] -> Int -> Int
+checkMirror (a:b:x) i = if a==b then i else checkMirror (b:x) (i+1)
+checkMirror [] _ = -1
+
+checkEither :: [[Char]] -> Int
+checkEither x
+    | horizontal /= -1 = horizontal*100
+    | otherwise = vertical
+    where
+        horizontal = checkMirror x 1
+        vertical = checkMirror (traspose x) 1
+
+getMatrices :: [T.Text] -> [[[Char]]] -> [[[Char]]]
+getMatrices (x:xs) (a:as)
+    | x==(T.pack "") = getMatrices xs ([]:a:as)
+    | otherwise = getMatrices xs ((a++[(T.unpack x)]):as)
+
+solve :: T.Text -> Int
+solve x = foldr (+) 0 $ map (checkEither) matrices
+    where
+        matrices = getMatrices $ T.lines x
