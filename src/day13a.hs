0ec4d049eaf03308b4cbb0dad3c510529fd019bd
Pablo Hernandez Almudi <pablo.hernandezalmudi@arm.com>
2023-12-13 20:05:22 +0000

    Day13 part 1

--- src/day13a.hs
+++ src/day13a.hs
@@ -2,30 +2,36 @@ module Day13a (solve) where
 
 import qualified Data.Text as T
 import Debug.Trace
+import Data.List
 
-checkMirrorAux :: [[Char]] -> [[Char]] -> Int
-checkMirrorAux (a:as) (b:bs)
-    | a==b = checkMirrorAux as bs
-    | otherwise = -1
+checkMirrorAux :: [[Char]] -> [[Char]] -> Int -> Int
+checkMirrorAux (a:as) (b:bs) i
+    | a==b = checkMirrorAux as bs i
+    | otherwise = 0
+checkMirrorAux _ _ i = i
 
-checkMirror :: [[Char]] -> Int -> Int
-checkMirror (a:b:x) i = if a==b then i else checkMirror (b:x) (i+1)
-checkMirror [] _ = -1
+checkMirror :: [[Char]] -> [[Char]] -> Int -> Int
+checkMirror (a:b:x) f i = if a==b then let r=(checkMirrorAux (reverse f) (x) i) in if r==0 then (checkMirror (b:x) (f++[a]) (i+1)) else r else checkMirror (b:x) (f++[a]) (i+1)
+checkMirror x y z = 0
+checkMirror [] _ _ = 0
 
 checkEither :: [[Char]] -> Int
-checkEither x
-    | horizontal /= -1 = horizontal*100
-    | otherwise = vertical
+checkEither x = (horizontal*100)+vertical
     where
-        horizontal = checkMirror x 1
-        vertical = checkMirror (traspose x) 1
+        horizontal = checkMirror x [] 1
+        vertical = checkMirror (transpose x) [] 1
 
 getMatrices :: [T.Text] -> [[[Char]]] -> [[[Char]]]
 getMatrices (x:xs) (a:as)
     | x==(T.pack "") = getMatrices xs ([]:a:as)
     | otherwise = getMatrices xs ((a++[(T.unpack x)]):as)
+getMatrices [] a = a
+getMatrices (x:xs) [] = getMatrices xs [[(T.unpack x)]]
+
+adder :: Int -> Int -> Int
+adder b a = (a+b)
 
 solve :: T.Text -> Int
-solve x = foldr (+) 0 $ map (checkEither) matrices
+solve x = foldr (adder) 0 $ map (checkEither) matrices
     where
-        matrices = getMatrices $ T.lines x
+        matrices = getMatrices (T.lines x) []
--- src/day13b.hs
+++ src/day13b.hs
@@ -1,6 +1,37 @@
 module Day13b (solve) where
 
-import Data.Text as T
+import qualified Data.Text as T
+import Debug.Trace
+import Data.List
 
-solve :: Text -> Int
-solve x = 0
+checkMirrorAux :: [[Char]] -> [[Char]] -> Int -> Int
+checkMirrorAux (a:as) (b:bs) i
+    | a==b = checkMirrorAux as bs i
+    | otherwise = 0
+checkMirrorAux _ _ i = i
+
+checkMirror :: [[Char]] -> [[Char]] -> Int -> Int
+checkMirror (a:b:x) f i = if a==b then let r=(checkMirrorAux (reverse f) (x) i) in if r==0 then (checkMirror (b:x) (f++[a]) (i+1)) else r else checkMirror (b:x) (f++[a]) (i+1)
+checkMirror x y z = 0
+checkMirror [] _ _ = 0
+
+checkEither :: [[Char]] -> Int
+checkEither x = (horizontal*100)+vertical
+    where
+        horizontal = checkMirror x [] 1
+        vertical = checkMirror (transpose x) [] 1
+
+getMatrices :: [T.Text] -> [[[Char]]] -> [[[Char]]]
+getMatrices (x:xs) (a:as)
+    | x==(T.pack "") = getMatrices xs ([]:a:as)
+    | otherwise = getMatrices xs ((a++[(T.unpack x)]):as)
+getMatrices [] a = a
+getMatrices (x:xs) [] = getMatrices xs [[(T.unpack x)]]
+
+adder :: Int -> Int -> Int
+adder b a = (a+b)
+
+solve :: T.Text -> Int
+solve x = foldr (adder) 0 $ map (checkEither) matrices
+    where
+        matrices = getMatrices (T.lines x) []
