3f7806599277827717dd256d1da92e3cdfb635ce
pablololo12 <pabloheralm@gmail.com>
2023-12-19 22:37:53 +0100

    Day19

--- src/day19a.hs
+++ src/day19a.hs
@@ -1,6 +1,77 @@
 module Day19a (solve) where
 
-import Data.Text as T
+import qualified Data.Text as T
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.List
+import Debug.Trace
 
-solve :: Text -> Int
-solve x = 0
+data Var = X | M | A | S | Z deriving (Eq,Ord,Show)
+type Entry = (Var,(Int->Int->Bool),Int,String)
+type Input = M.Map Var Int
+
+noop :: Int -> Int -> Bool
+noop _ _ = True
+
+parseVar :: Char -> Var
+parseVar 'x' = X
+parseVar 'm' = M
+parseVar 'a' = A
+parseVar 's' = S
+
+parseEntry :: T.Text -> Entry
+parseEntry x
+  | (not (':' `T.elem` x)) = (Z,noop,0,(T.unpack x))
+  | gt = ((parseVar (T.index (head col) 0)),(>),(read $ T.unpack $ last operation),(T.unpack $ last col))
+  | lt = ((parseVar (T.index (head col) 0)),(<),(read $ T.unpack $ last operation),(T.unpack $ last col))
+  | otherwise = (Z,noop,0,(T.unpack x))
+  where
+    col = T.split (==':') x
+    gt = '>' `T.elem` x
+    lt = '<' `T.elem` x
+    operation = T.split (\w -> w=='<'||w=='>') $ head col
+
+parseRules :: [T.Text] -> [(String,[Entry])]
+parseRules [] = []
+parseRules (x:xs) = (indx,rules):parseRules xs
+  where
+    indx = T.unpack $ head sp1
+    rules = map (parseEntry) $ T.split (==',') sp2
+    sp1 = T.split (=='{') x
+    sp2 = head $ T.split (=='}') $ last sp1
+
+getIn :: T.Text -> (Var,Int)
+getIn x = (a,b)
+  where
+    a = parseVar $ T.index x 0
+    b = read $ T.unpack $ last $ T.split (=='=') x
+
+parseInputs :: [T.Text] -> [Input]
+parseInputs [] = []
+parseInputs (x:xs) = (M.fromList $ map (getIn) clean):parseInputs xs
+  where
+    clean = T.split (==',') $ T.drop 1 $ T.dropEnd 1 x
+
+algoAux :: [Entry] -> Input -> String
+algoAux (x@(a,f,b,c):xs) i = if (f (i M.! a) b) then c else algoAux xs i
+
+algo :: M.Map String [Entry] -> String -> Input -> Bool
+algo _ "A" _ = True
+algo _ "R" _ = False
+algo ent ind inp = algo ent getN inp
+  where
+    ents = ent M.! ind
+    getN = algoAux ents inp
+
+sumEnt :: Input -> Int
+sumEnt i = (i M.! X)+(i M.! M)+(i M.! A)+(i M.! S)
+
+solve :: T.Text -> Int
+solve x = traceShow (accept) foldl (\a (c,d) -> if d then a+c else a) 0 $ zip count accept
+  where
+    count = map (sumEnt) inputs
+    accept = map (\w -> algo rules "in" w) inputs
+    rules = M.fromList $ parseRules $ fst lines
+    inputs = parseInputs $ tail $ snd lines
+    lines = splitAt wheretosplit $ T.lines x
+    wheretosplit = fromJust $ elemIndex (T.pack "") $ T.lines x
--- src/day19b.hs
+++ src/day19b.hs
@@ -1,6 +1,70 @@
 module Day19b (solve) where
 
-import Data.Text as T
+import qualified Data.Text as T
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.List
+import Debug.Trace
 
-solve :: Text -> Int
-solve x = 0
+data Var = X | M | A | S | Z deriving (Eq,Ord,Show)
+data Op = GGT | LLT | T deriving (Eq,Ord,Show)
+type Entry = (Var,Op,Int,String)
+type Input = M.Map Var [Int]
+
+parseVar :: Char -> Var
+parseVar 'x' = X
+parseVar 'm' = M
+parseVar 'a' = A
+parseVar 's' = S
+
+parseEntry :: T.Text -> Entry
+parseEntry x
+  | (not (':' `T.elem` x)) = (Z,T,0,(T.unpack x))
+  | gt = ((parseVar (T.index (head col) 0)),GGT,(read $ T.unpack $ last operation),(T.unpack $ last col))
+  | lt = ((parseVar (T.index (head col) 0)),LLT,(read $ T.unpack $ last operation),(T.unpack $ last col))
+  | otherwise = (Z,T,0,(T.unpack x))
+  where
+    col = T.split (==':') x
+    gt = '>' `T.elem` x
+    lt = '<' `T.elem` x
+    operation = T.split (\w -> w=='<'||w=='>') $ head col
+
+parseRules :: [T.Text] -> [(String,[Entry])]
+parseRules [] = []
+parseRules (x:xs) = (indx,rules):parseRules xs
+  where
+    indx = T.unpack $ head sp1
+    rules = map (parseEntry) $ T.split (==',') sp2
+    sp1 = T.split (=='{') x
+    sp2 = head $ T.split (=='}') $ last sp1
+
+-- Done parsing
+
+algoAux :: [Entry] -> Input -> [(String,Input)]
+algoAux (x@(a,f,b,c):xs) i
+  | f==GGT && (head (i M.! a))>b = [(c,i)]
+  | f==LLT && (last (i M.! a))<b = [(c,i)]
+  | f==GGT && (last (i M.! a))<b = algoAux xs i
+  | f==LLT && (head (i M.! a))>b = algoAux xs i
+  | f==GGT = (c, (M.insert a (dropWhile (<=b) (i M.! a)) i)):algoAux xs (M.insert a (takeWhile (<b) (i M.! a)) i)
+  | f==LLT = (c, (M.insert a (takeWhile (<b) (i M.! a)) i)):algoAux xs (M.insert a (dropWhile (<=b) (i M.! a)) i)
+  | otherwise = [(c,i)]
+
+sumEnt :: Input -> Int
+sumEnt i = (length (i M.! X))*(length (i M.! M))*(length (i M.! A))*(length (i M.! S))
+
+algo :: M.Map String [Entry] -> String -> Input -> Int
+algo _ "A" i = sumEnt i
+algo _ "R" _ = 0
+algo ent ind inp = foldl (\a (s,e) -> a + (algo ent s e)) 0 getN
+  where
+    ents = ent M.! ind
+    getN = algoAux ents inp
+
+solve :: T.Text -> Int
+solve x = accept
+  where
+    accept = algo rules "in" $ M.fromList [(X,[1..4000]),(M,[1..4000]),(A,[1..4000]),(S,[1..4000])]
+    rules = M.fromList $ parseRules $ fst lines
+    lines = splitAt wheretosplit $ T.lines x
+    wheretosplit = fromJust $ elemIndex (T.pack "") $ T.lines x
